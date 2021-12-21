47d6179858e5ad567b11114599ad423a20a389ec
Sahab <sahabferdous1@gmail.com>
2021-12-22 07:48:54 +0800

    Added React Navigation

---
+++ screens/SignInScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SignInScreen = () => {
+  return (
+    <View>
+      <Text>Sign in page</Text>
+    </View>
+  );
+};
+
+export default SignInScreen;
+
+const styles = StyleSheet.create({});

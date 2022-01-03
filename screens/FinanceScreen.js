f2dda625f3fb8546e9dcc892952ed217c204c958
Sahab <sahabferdous1@gmail.com>
2022-01-03 08:04:23 +0800

    Added logo's to each page

--- screens/FinanceScreen.js
+++ screens/FinanceScreen.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Icon } from "react-native-elements";
 
 const FinanceScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Icon
+        name="dollar-sign"
+        type="feather"
+        size={28}
+        color="gray"
+        style={{ marginBottom: 10 }}
+      />
       <Text>Finance tracker (coming soon)</Text>
     </View>
   );
--- screens/PomodoroScreen.js
+++ screens/PomodoroScreen.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Icon } from "react-native-elements";
 
 const Pomodoro = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Icon
+        name="clock"
+        type="feather"
+        size={28}
+        color="gray"
+        style={{ marginBottom: 10 }}
+      />
       <Text>Pomodoro timer (coming soon)</Text>
     </View>
   );
